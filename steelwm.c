e5663671045b0bc1664524cfd83e5b85be372398
Aceroph <aos92@proton.me>
2025-08-06 08:40:30 -0400

    Add GPL license

--- steelwm.c
+++ steelwm.c
@@ -1,3 +1,19 @@
+/* SteelWM, a dynamic tiling window manager based on DWM
+ * Copyright (C) 2025  Aceroph
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
